603429575aa2c5a7a70bde79897986eeedbee644
Luis Leandro <elio@codepoint.pt>
2021-11-27 17:40:35 +0000

    chore: add not found page for redirection of bad endpoints

--- src/pages/notFound/NotFoundPage.js
+++ src/pages/notFound/NotFoundPage.js
@@ -1,5 +1,42 @@
 import React from 'react'
 
-const LoginPage = () => <div>Not Found</div>
+import {useHistory} from 'react-router'
 
-export default LoginPage
+import PageNotFound from '../../assets/page_not_found.svg'
+import {
+  Margin,
+  StyledNotFoundContainer,
+  StyledTitle,
+  StyledDescription,
+  StyledNotFoundLogo,
+  StyledNotFoundGroup,
+  MarginBackNotFound,
+  BackButton
+} from './NotFoundStyles'
+
+const NotFound = () => {
+  const history = useHistory()
+
+  return (
+    <StyledNotFoundContainer>
+      <StyledNotFoundGroup>
+        <StyledNotFoundLogo src={PageNotFound} />
+        <Margin size={30} />
+        <StyledTitle>Oops... Page not found!</StyledTitle>
+        <Margin size={30} />
+        <StyledDescription>
+          We apologize for the inconvenience. <br /> Apparently, the page
+          that you are trying to find has been erased or never existed.
+        </StyledDescription>
+        <Margin size={40} />
+        <MarginBackNotFound>
+          <BackButton onClick={() => history.push('/')}>
+            Back to Home
+          </BackButton>
+        </MarginBackNotFound>
+      </StyledNotFoundGroup>
+    </StyledNotFoundContainer>
+  )
+}
+
+export default NotFound
