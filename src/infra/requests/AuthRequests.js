947d1b783bbefcc101751b579c78552735049311
luisleandro <luisleandro.wd@gmail.com>
2021-12-16 19:31:38 +0000

    refactor: change validations on signup form

--- src/infra/requests/AuthRequests.js
+++ src/infra/requests/AuthRequests.js
@@ -1,3 +1,5 @@
 import client from '../config/AxiosConfig'
 
 export const Login = async (data) => client.post('/v1/auth', data)
+
+export const SignUp = async (data) => client.post('/v1/auth/signup', data)
--- src/shared/components/signupForm/SignupForm.js
+++ src/shared/components/signupForm/SignupForm.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
+import {message} from 'antd'
 import {Form} from 'react-final-form'
+import {useHistory} from 'react-router'
 
+import {SignUp} from '../../../infra/requests/AuthRequests'
+import FormValidator from '../../../infra/services/validations/FormValidator'
 import {
   BodyWrapper,
   ButtonContainer,
@@ -25,83 +29,110 @@ const onSubmit = (values) => {
   console.log(values)
 }
 
-const validate = (values) => {
-  if (!values) {
-    console.log('erro')
-  } else {
-    console.log('ok')
-  }
-}
+const validate = FormValidator.make({
+  email: 'required|email',
+  password: 'required',
+  firstName: 'required',
+  lastName: 'required',
+  postalCode: 'required'
+})
 
-const SignUpForm = () => (
-  <BodyWrapper>
-    <FormWrapper>
-      <FormTitle>Sign up for PetAway</FormTitle>
-      <Form
-        onSubmit={onSubmit}
-        validate={validate}
-        render={({handleSubmit, submitting}) => (
-          <FormContainer onSubmit={handleSubmit}>
-            <InputWrapper>
-              <InputTitle>First Name</InputTitle>
-              <InputField
-                name='firstName'
-                component={SignupInput}
-                type='text'
-              />
-            </InputWrapper>
-            <InputWrapper>
-              <InputTitle>Last Name</InputTitle>
-              <InputField
-                name='lastName'
-                component={SignupInput}
-                type='text'
-              />
-            </InputWrapper>
-            <InputWrapper>
-              <InputTitle>Postal Code</InputTitle>
-              <InputField
-                name='postalCode'
-                component={SignupInput}
-                type='text'
-              />
-            </InputWrapper>
-            <InputWrapper>
-              <InputTitle>E-Mail</InputTitle>
-              <InputField
-                name='email'
-                component={SignupInput}
-                type='text'
-              />
-            </InputWrapper>
-            <InputWrapper>
-              <InputTitle>Create Password</InputTitle>
-              <InputField
-                name='password'
-                component={PasswordInput}
-                type='text'
-              />
-            </InputWrapper>
-            <Disclaimer>
-              By signing in or signing up, I agree to PetAway.com's{' '}
-              <DisclaimerLinks>Terms of Service</DisclaimerLinks> and{' '}
-              <DisclaimerLinks>Privacy Policy</DisclaimerLinks>, confirm
-              that I am 18 years of age or older, and consent to receiving
-              email communication.
-            </Disclaimer>
-            <ButtonContainer>
-              <SignUpButton disabled={submitting}>Sign Up</SignUpButton>
-            </ButtonContainer>
-          </FormContainer>
-        )}
-      />
-      <Separator />
-      <LoginRedirect>
-        Already have a PetAway account?{' '}
-        <LoginRedirectLink href='/login'>Sign in now.</LoginRedirectLink>
-      </LoginRedirect>
-    </FormWrapper>
-  </BodyWrapper>
-)
+const SignUpForm = () => {
+  const history = useHistory()
+  const SignUpResponse = async (data) => {
+    try {
+      const response = await SignUp(data)
+
+      if (response.success) {
+        history.push('/')
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const failure = (error) => {
+    message.error({
+      content: `${error}`,
+      className: 'custom-class',
+      style: {
+        marginTop: '20vh'
+      },
+      maxCount: 1
+    })
+  }
+
+  return (
+    <BodyWrapper>
+      <FormWrapper>
+        <FormTitle>Sign up for PetAway</FormTitle>
+        <Form
+          onSubmit={SignUpResponse}
+          validate={validate}
+          render={({handleSubmit, submitting, values}) => (
+            <FormContainer onSubmit={handleSubmit}>
+              <InputField name='firstName'>
+                {({input, meta}) => (
+                  <InputWrapper>
+                    <InputTitle>First Name</InputTitle>
+                    <SignupInput {...input} type='text' meta={meta} />
+                  </InputWrapper>
+                )}
+              </InputField>
+              <InputField name='lastName'>
+                {({input, meta}) => (
+                  <InputWrapper>
+                    <InputTitle>Last Name</InputTitle>
+                    <SignupInput {...input} type='text' meta={meta} />
+                  </InputWrapper>
+                )}
+              </InputField>
+              <InputField name='postalCode'>
+                {({input, meta}) => (
+                  <InputWrapper>
+                    <InputTitle>Postal Code</InputTitle>
+                    <SignupInput {...input} type='text' meta={meta} />
+                  </InputWrapper>
+                )}
+              </InputField>
+              <InputField name='email'>
+                {({input, meta}) => (
+                  <InputWrapper>
+                    <InputTitle>E-Mail</InputTitle>
+                    <SignupInput {...input} type='text' meta={meta} />
+                  </InputWrapper>
+                )}
+              </InputField>
+              <InputField name='password'>
+                {({input, meta}) => (
+                  <InputWrapper>
+                    <InputTitle>Create Password</InputTitle>
+                    <PasswordInput {...input} meta={meta} />
+                  </InputWrapper>
+                )}
+              </InputField>
+              <Disclaimer>
+                By signing in or signing up, I agree to PetAway.com's{' '}
+                <DisclaimerLinks>Terms of Service</DisclaimerLinks> and{' '}
+                <DisclaimerLinks>Privacy Policy</DisclaimerLinks>, confirm
+                that I am 18 years of age or older, and consent to
+                receiving email communication.
+              </Disclaimer>
+              <ButtonContainer>
+                <SignUpButton disabled={submitting}>Sign Up</SignUpButton>
+              </ButtonContainer>
+              <pre>{JSON.stringify(values, 0, 2)}</pre>
+            </FormContainer>
+          )}
+        />
+        <Separator />
+        <LoginRedirect>
+          Already have a PetAway account?{' '}
+          <LoginRedirectLink href='/login'>Sign in now.</LoginRedirectLink>
+        </LoginRedirect>
+      </FormWrapper>
+    </BodyWrapper>
+  )
+}
 
 export default SignUpForm
