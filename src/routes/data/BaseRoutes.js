6fd74229709076cb4441b72e04c0c569d936dc15
Luís Leandro <luisleandro.wd@gmail.com>
2022-01-22 01:12:59 +0000

    refactor: add validations for auth routes to prevent unauthorized access

--- src/routes/data/BaseRoutes.js
+++ src/routes/data/BaseRoutes.js
@@ -1,42 +1,26 @@
 import Search from '../../pages/search/Search'
-import Services from '../../pages/services/Services'
 import UserProfile from '../../pages/userProfile/UserProfile'
 import BecomeWalkerForm from '../../shared/components/becomeWalkerForm/BecomeWalkerForm'
-import SignUpForm from '../../shared/components/signupForm/SignupForm'
 
 export default [
   {
-    path: '/signup',
-    component: SignUpForm,
-    isAuthenticated: false,
-    header: true,
-    role: 1
-  },
-  {
-    path: '/services',
-    component: Services,
-    isAuthenticated: false,
-    header: true,
-    role: 1
-  },
-  {
     path: '/profile',
     component: UserProfile,
-    isAuthenticated: false,
+    isAuthenticated: true,
     header: true,
     role: 1
   },
   {
     path: '/becomeWalker',
     component: BecomeWalkerForm,
-    isAuthenticated: false,
+    isAuthenticated: true,
     header: true,
     role: 1
   },
   {
     path: '/search',
     component: Search,
-    isAuthenticated: false,
+    isAuthenticated: true,
     header: true,
     role: 1
   }
